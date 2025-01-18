0d38d2aec9792f27c7fd8f84bdd3418110ed752a
Ben Urquhart <benurquhart11@gmail.com>
2025-01-18 17:46:00 +1300

    Added macros

--- ivm.h
+++ ivm.h
@@ -14,7 +14,7 @@ namespace ivm {
 
 			uint8_t _padd[8];
 		};
-		
+
 		constexpr auto IVM_MOV = 1;
 		constexpr auto IVM_ADD = 2;
 		constexpr auto IVM_SUB = 3;
@@ -24,7 +24,6 @@ namespace ivm {
 		constexpr auto IVM_CMP = 7;
 		constexpr auto IVM_JNE = 8;
 		constexpr auto IVM_CNA = 9;
-		constexpr auto IVM_EXIT = 10;
 
 		constexpr auto IVM_DEREF_NONE = 0;
 		constexpr auto IVM_DEREF_SRC = 1;
@@ -73,81 +72,52 @@ namespace ivm {
 	constexpr auto R4 = ivm_rgstr(5);
 	constexpr auto R5 = ivm_rgstr(6);
 
-	constexpr auto EXIT() -> ivm_rgstr {
-
-		return ivm_rgstr(IVM_EXIT ^ ivm_seed);
+	#define DEFINE_INSTR(name, opcode)								\
+	template <typename T = ivm_default>								\
+	constexpr auto name(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0)		\
+		-> uint16_t {												\
+		return build_instr<T>(rgstr1, rgstr2, opcode);				\
 	}
 
-	template <typename T = ivm_default>
-	constexpr auto MOV(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0) -> ivm_instr {
+	DEFINE_INSTR(MOV, IVM_MOV)
+	DEFINE_INSTR(ADD, IVM_ADD)
+	DEFINE_INSTR(SUB, IVM_SUB)
+	DEFINE_INSTR(XOR, IVM_XOR)
+	DEFINE_INSTR(LEA, IVM_LEA)
+	DEFINE_INSTR(CMP, IVM_CMP)
+	DEFINE_INSTR(JNE, IVM_CMP, 0)
+	DEFINE_INSTR(CNA, IVM_CMP, 0)
 
-		return build_instr<T>(rgstr1, rgstr2, IVM_MOV);
-	}
+	#undef DEFINE_INSTR
 
-	template <typename T = ivm_default>
-	constexpr auto ADD(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr2, IVM_ADD);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto SUB(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr2, IVM_SUB);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto XOR(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr2, IVM_XOR);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto LEA(ivm_rgstr rgstr1, ivm_rgstr rgstr2) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr2, IVM_LEA);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto CMP(ivm_rgstr rgstr1, ivm_rgstr rgstr2 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr2, IVM_CMP);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto JNE(ivm_rgstr rgstr1 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr1, IVM_JNE);
-	}
-
-	template <typename T = ivm_default>
-	constexpr auto CNA(ivm_rgstr rgstr1 = 0) -> ivm_instr {
-
-		return build_instr<T>(rgstr1, rgstr1, IVM_CNA);
-	}
-
+	#define GET_IMMV(instr) (((instr) >> 7) & IVM_IMMV)
+	#define GET_SIZE(instr) ((((instr) >> 4) & 0x7) + 1)
+	#define GET_DEREF(oprnd) (((oprnd) >> 6) & 0x3)
+	#define GET_RGSTR1(oprnd) ((oprnd) & 0x7)
+	#define GET_RGSTR2(oprnd) (((oprnd) >> 3) & 0x7)
+		
 	//
 
-	auto ivm(const std::vector<ivm_instr> &prgm, bool dbg = true) -> void {
+	auto ivm(const std::vector<ivm_instr>& prgm, bool dbg = true) -> void {
 
 		auto prgm_counter = 0, zf = 0;
 
 		ivm_rgstr rgstr[6] = { 0 };
 
-		while(prgm_counter < prgm.size()) {
+		while (prgm_counter < prgm.size()) {
 
 			const auto prgm_instr = ((unsigned short)(prgm[prgm_counter]) ^ ivm_seed);
 
 			const auto instr = (unsigned char)(prgm_instr & 0xFF);
 			const auto oprnd = (unsigned char)((prgm_instr >> 8) & 0xFF);
 
-			const auto immv = ((instr >> 7) & IVM_IMMV);
-			const auto size = ((instr >> 4) & 0x7) + 1;
+			const auto immv = GET_IMMV(instr);
+			const auto size = GET_SIZE(instr);
 
-			const auto deref = (oprnd >> 6) & 0x3;
+			const auto deref = GET_DEREF(oprnd);
 
-			const auto rgstr1 = (oprnd & 0x7);
-			const auto rgstr2 = (oprnd >> 3) & 0x7;
+			const auto rgstr1 = GET_RGSTR1(oprnd);
+			const auto rgstr2 = GET_RGSTR2(oprnd);
 
 			auto src = immv ? &prgm[prgm_counter + 1] : &rgstr[rgstr2];
 
